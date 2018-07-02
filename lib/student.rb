eb9ce0f200b742aac21de1bb62594250779a393a
Jed Peek <jedpeek@yahoo.com>
2018-07-02 20:33:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,9 +37,9 @@ class Student
       LIMIT ?
       SQL
 
-      DB[:conn].execute(sql, x).map{|row| self.new_from_db(row)}.first
+      DB[:conn].execute(sql, x).map{|row| self.new_from_db(row)}
   end
-    
+
 
   def save
     sql = <<-SQL
