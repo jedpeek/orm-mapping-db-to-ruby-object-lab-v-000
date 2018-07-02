8ec47bebfced9e239c89f3308d12f7004935016b
Jed Peek <jedpeek@yahoo.com>
2018-07-02 20:33:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
       DB[:conn].execute(sql, x).map{|row| self.new_from_db(row)}
   end
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
