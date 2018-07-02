17d12cea0826a14f44b0738705c163c5b0f409b2
Jed Peek <jedpeek@yahoo.com>
2018-07-02 20:38:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,6 +71,16 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT COUNT(id)
+    FROM students
+    WHERE grade != 12
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
